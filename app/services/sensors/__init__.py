70617fd5be9f7f463ba29006704ab8e06a53e9e2
Marcos Alonso <marcosalonsoamor05@gmail.com>
2025-10-26 13:42:48 +0100

    Initial commit

---
+++ app/services/sensors/__init__.py
@@ -0,0 +1,9 @@
+from .motion_sensor import MotionSensor
+from .temperature_sensor import TemperatureSensor
+from .access_sensor import AccessSensor
+
+SENSOR_REGISTRY = {
+    "motion": MotionSensor(),
+    "temperature": TemperatureSensor(),
+    "access": AccessSensor(),
+}
